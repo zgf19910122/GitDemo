8b2d70332bb8e19c0d91c95591058421fdd70072
红眼的帅哥 <1583556059@qq.com>
2016-04-20 00:30:10 +0800

    提交测试

--- app/src/main/java/com/example/fly/asdemo/MainActivity.java
+++ app/src/main/java/com/example/fly/asdemo/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.fly.asdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int i = 1;
     }
 }
